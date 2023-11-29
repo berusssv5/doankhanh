24b7e57cf6eed6d4feff1373979317bcd2532179
Đức 3K <63569635+berusssv5@users.noreply.github.com>
2023-11-29 19:50:47 +0700

    Update and rename README.md to miningcoin.sh

---
+++ miningcoin.sh
@@ -0,0 +1,2 @@
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
+&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3S9Y6AstMLrqAFzq34429wPQcJKYKJuzGpWBA89Fd32qRGJ7UVPWS65M9Erp79km23nHHuHerbsaBjar8oDTXZyDTmH9i2G -p duc3k -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
